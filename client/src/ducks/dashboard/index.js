6c3793c67bd0217eb9b541e1dc7d1106bb113466
SlavalS <slavaavals616@gmail.com>
2018-11-10 11:55:17 +0400

    added ci linter

--- client/src/ducks/dashboard/index.js
+++ client/src/ducks/dashboard/index.js
@@ -3,16 +3,14 @@ import type {ActionT} from '../../typedefs';
 
 export const SUBMIT_EDIT_TITLE = 'SUBMIT_EDIT_TITLE';
 
-export const submitEditTitle = title => ({
+export const submitEditTitle = (title: string) => ({
 	payload: title,
 	type   : SUBMIT_EDIT_TITLE,
 });
 
 const
-	initialState = {
-		title: '',
-	};
+	initialState = {title: ''};
 
-export const reducer = (state = initialState, action: ActionT) => {
-	console.log(action);
+export const reducer = (state: Object = initialState, action: ActionT) => {
+	console.log(state, action);
 };
